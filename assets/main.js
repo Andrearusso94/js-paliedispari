7746ccecb5b4ff3d92e263bae039acbd471dd608
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-14 12:15:24 +0200

    add random number for pc

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,22 @@
 //Pari e Dispari
 
-//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri 
+//. Sommiamo i due numeri 
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 //Dichiariamo chi ha vinto.
 
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
 
-const myNumber = Number(prompt('scegli un numero da 1 a 5'))
+const myNumber = Number(prompt('scegli un numero da 1 a 5'));
+console.log(myNumber, 'questo è il mio numero')
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+
+function generateRandomNumber(min, max){
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+
+for (let i = 0; i < 1; i++){
+    const randomNumber = generateRandomNumber(1)
+    console.log(randomNumber, 'questo è il numero del computer')
+
+}
